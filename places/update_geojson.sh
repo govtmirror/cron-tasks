5d1326707014a30a1fdca7ec85b2258f9602b77c
Jim McAndrew <jim@loc8.us>
2014-11-19 15:49:04 -0700

    Use temp file

--- places/update_geojson.sh
+++ places/update_geojson.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 rootdir=`pwd`
+todays_date=`date "+%Y%m%d"`
 cd "/home/npmap/dev/cron-tasks/places"
 format="geojson"
 output="/home/npmap/dev/data/places/points_of_interest.geojson"
+output_temp="/home/npmap/dev/data/places/points_of_interest_"$todays_date".geojson"
 sqlFile="get_nodes.sql"
-commitMessage="Updating the points_of_interest.geojson for `date "+%Y%m%d"`"
+commitMessage="Updating the points_of_interest.geojson for $todays_date"
 
-/bin/bash ./export.sh -f "$format" -o "$output" -s "$sqlFile" && /bin/bash git_commit_file "$output"
+/bin/bash ./export.sh -f "$format" -o "$output" -s "$sqlFile" && rm $output && mv &output_temp $output && /bin/bash git_commit_file "$output"
 cd $rootdir
