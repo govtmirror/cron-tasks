72ac3449cb0d092e9eab58534325aeda79300a3b
Jim McAndrew <jim@loc8.us>
2014-11-20 13:57:22 -0700

    Update update_geojson.sh

--- tasks/places/update_geojson.sh
+++ tasks/places/update_geojson.sh
@@ -8,5 +8,11 @@ output_temp="/home/npmap/dev/data/places/points_of_interest_"$todays_date".geojs
 sqlFile="get_nodes.sql"
 commitMessage="Updating the points_of_interest.geojson for $todays_date"
 
-/bin/bash ./export.sh -f "$format" -o "$output_temp" -s "$sqlFile" && rm $output && mv $output_temp $output && /bin/bash ./git_commit_file.sh "$output" "$commitMessage"
+# Do a git fetch for the repo where we're writing
+cd dirname $output
+git reset --hard origin
+cd $rootdir
+
+
+/bin/bash $rootdir/export.sh -f "$format" -o "$output_temp" -s "$sqlFile" && rm $output && mv $output_temp $output && /bin/bash $rootdir/git_commit_file.sh "$output" "$commitMessage"
 cd $rootdir
