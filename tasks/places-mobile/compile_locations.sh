768a9144ad6a948ba1664f4f2b0274d62ab9ff6d
Jim McAndrew <jim@loc8.us>
2014-11-20 16:43:12 -0700

    Update compile_locations.sh

--- tasks/places-mobile/compile_locations.sh
+++ tasks/places-mobile/compile_locations.sh
@@ -5,7 +5,7 @@ site_generation_directory=/home/npmap/dev/website/places-mobile/aggregation/
 site_generation_script=generate_sites.sh
 git_commit_script=git_commit_changes.sh
 cd $site_generation_directory
-bash $site_generation_script $1
+/bin/bash $site_generation_script $1 &>/dev/null
 
 # Commit the changes to github
 bash $git_commit_script
