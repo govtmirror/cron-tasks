47d4aa683303de27d15ff87256cb754b39cc466b
Jim McAndrew <jim@loc8.us>
2014-11-20 15:09:15 -0700

    Turn off all tasks for now

--- src/taskList.js
+++ src/taskList.js
@@ -14,7 +14,7 @@ module.exports = function(params) {
       'path': handlebars('/bin/bash {{tasksDir}}/test/db_test.sh'),
     }
   }, {
-    'enabled': true,
+    'enabled': false,
     'interval': '0 30 1 * * *', // 1:30 am
     'name': 'Places_SQL_Dump',
     'task': {
@@ -22,7 +22,7 @@ module.exports = function(params) {
       'path': handlebars('/bin/bash {{tasksDir}}/places/sql_dump.sh'),
     }
   }, {
-    'enabled': true,
+    'enabled': false,
     'interval': '0 15 * * * *', // On the 15 of every hour
     'name': 'Places_POI_update',
     'task': {
@@ -30,7 +30,7 @@ module.exports = function(params) {
       'path': handlebars('/bin/bash {{tasksDir}}/places/update_geojson.sh'),
     }
   }, {
-    'enabled': true,
+    'enabled': false,
     'interval': '0 30 * * * *', // On the 30 of every hour
     'name': 'Places_Mobile_Data_Only',
     'task': {
@@ -38,7 +38,7 @@ module.exports = function(params) {
       'path': handlebars('/bin/bash {{tasksDir}}/places-mobile/compile_locations.sh'),
     }
   }, {
-    'enabled': true,
+    'enabled': false,
     'interval': '0 45 */12 * * *', // Every 12 hours on the 45
     'name': 'Places_Mobile_Images',
     'task': {
