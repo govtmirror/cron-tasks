fa99d24fa07f052f85e621132eb407ae0b25d5d9
Jim McAndrew <jim@loc8.us>
2015-02-17 14:20:34 -0700

    rename dirs

--- tasks/places-mobile/compile_locations.sh
+++ tasks/places-mobile/compile_locations.sh
@@ -1,7 +1,7 @@
 script_dir=`pwd`
 
 # Generate the sites
-site_generation_directory=/home/npmap/dev/website/tools/places/mobile/aggregation/
+site_generation_directory=/home/npmap/dev/website/tools/places/mobile/aggregate/aggregation/
 site_generation_script=generate_sites.sh
 git_commit_script=git_commit_changes.sh
 cd $site_generation_directory
