70200c60623d68298f2ad24243f281ec8c83db0b
Jim McAndrew <jim@loc8.us>
2015-02-17 14:07:05 -0700

    Change paths

--- tasks/places-backup/update_geojson.sh
+++ tasks/places-backup/update_geojson.sh
@@ -10,7 +10,7 @@ commitMessage="Updating the points_of_interest.geojson for $todays_date"
 # Do a git fetch for the repo where we're writing
 cd `dirname $output`
 git reset --hard origin && git pull
-cd "/home/npmap/dev/cron-tasks/tasks/places"
+cd "/home/npmap/dev/cron-tasks/tasks/places-backup"
 
 /bin/bash ./export.sh -f "$format" -o "$output_temp" -s "$sqlFile" && rm $output && mv $output_temp $output && /bin/bash ./git_commit_file.sh "$output" "$commitMessage"
 
