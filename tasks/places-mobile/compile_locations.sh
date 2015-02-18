92651ec0b83812f01da14e7887530e4ea309bfc6
Jim McAndrew <jim@loc8.us>
2015-02-18 10:14:39 -0700

    Move the git keys to the scripts: Update tasks/places-mobile/compile_locations.sh

--- tasks/places-mobile/compile_locations.sh
+++ tasks/places-mobile/compile_locations.sh
@@ -5,6 +5,10 @@ data_directory=/home/npmap/dev/data/places_mobile
 git_commit_script=git_commit_changes.sh
 site_generation_directory=/home/npmap/dev/website/tools/places/mobile/aggregate/aggregation/
 site_generation_script=generate_sites.sh
+git_rsa_key=/home/npmap/.ssh/npmap-bot/id_rsa
+
+eval $(ssh-agent) > /dev/null && ssh-add $git_rsa_key
+
 cd $site_generation_directory
 /bin/bash $site_generation_script $1 > /dev/null
 
