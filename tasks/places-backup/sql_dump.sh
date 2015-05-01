420249087d87f57a6bcfa2fec04d940348c15d6c
Jim McAndrew <jim@loc8.us>
2015-05-01 13:48:38 -0600

    switch dbs back

--- tasks/places-backup/sql_dump.sh
+++ tasks/places-backup/sql_dump.sh
@@ -1,11 +1,9 @@
 this_pg_dump=/usr/lib/postgresql/9.4/bin/pg_dump
 output_dir="/home/npmap/dev/backups/"
 api_filename="api_bk_`date "+%Y%m%d"`.tar"
-render_filename="render_bk_`date "+%Y%m%d"`.tar"
-#validated_filename="validated_bk_`date "+%Y%m%d"`.tar"
+pgs_filename="poi_bk_`date "+%Y%m%d"`.tar"
 
 # TODO: These are just tar files, it'd be nice to use an order/uncompressed dump and compress only the deltas
 # We can also keep a single current one and put it on a server somewhere
-$this_pg_dump -Fc -U postgres -h 10.147.153.193 places_prod > $output_dir$api_filename
-$this_pg_dump -Fc -U postgres -h 10.147.153.193 places_render_prod > $output_dir$render_filename
-#$this_pg_dump -Fc -U postgres -h 10.147.153.193 places_validated_prod > $output_dir$validated_filename
+$this_pg_dump -Fc -U postgres -h 10.147.153.193 places_api > $output_dir$api_filename
+$this_pg_dump -Fc -U postgres -h 10.147.153.193 places_pgs > $output_dir$pgs_filename
