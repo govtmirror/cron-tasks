1f1ac2dd34c235326746a97603c5847620100efe
Jim McAndrew <jim@loc8.us>
2015-07-24 12:58:12 -0600

    Use multiple keys

--- tasks/cartodb-backup/download.sh
+++ tasks/cartodb-backup/download.sh
@@ -4,11 +4,11 @@
 function downloadTables ()
 {
   source "./settings.sh"
-  local api_key=$(cat $CartoDB_api_key_file)
   local destination=$CartoDB_destination
   local file_type=$CartoDB_file_type
   # local tables=$(cat $CartoDB_table_list_file)
   local downloadUrl=$CartoDB_downloadUrl
+  local api_key=""
   local account=""
   local file_name=""
   local table_name=""
@@ -25,14 +25,15 @@ function downloadTables ()
       newUrl=${newUrl/\{\{$field\}\}/$fieldVal}
     done
     echo "Downloading Table: $table_name to $destination/$file_name"
+    # echo $newUrl
     curl -L -o "$destination/$file_name" "$newUrl"
   }
 
   mkdir -p $destination
   while read line; do
-    echo "line" $line
-    account=$(echo $line| perl -pe "s/^(.+?),\s{0,}(.+?)$/\1/g")
-    table_name=$(echo $line| perl -pe "s/^(.+?),\s{0,}(.+?)$/\2/g")
+    account=$(echo $line | perl -pe "s/^(.+?),\s{0,}(.+?)$/\1/g")
+    table_name=$(echo $line | perl -pe "s/^(.+?),\s{0,}(.+?)$/\2/g")
+    api_key=$(grep -r "^$account," "$CartoDB_api_key_file" | perl -pe "s/^(.+?),\s{0,}(.+?)$/\2/g")
     file_name=$table_name"."$file_type
     downloadTable
   done < "$CartoDB_table_list_file"
