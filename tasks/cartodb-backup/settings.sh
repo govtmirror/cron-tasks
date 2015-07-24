78d66168edc963a90a2be3fc7ba4b543c9ce98dd
Jim McAndrew <jim@loc8.us>
2015-07-24 12:58:20 -0600

    Change backup dest

--- tasks/cartodb-backup/settings.sh
+++ tasks/cartodb-backup/settings.sh
@@ -2,7 +2,7 @@
 CartoDB_file_type="csv"
 CartoDB_api_key_file="./secrets/cartodb_api_key.txt"
 CartoDB_table_list_file="./tables.txt"
-CartoDB_destination="./backup"
+CartoDB_destination="/mnt/backups/cartodb"
 
 # everything in {{ }} brackets MUST have an associated variable in the download.sh
 CartoDB_downloadUrl="https://{{account}}.cartodb.com/api/v2/sql?filename={{file_name}}&q=SELECT+*+FROM+(select+*+from+{{table_name}})+as+subq+&format={{file_type}}&api_key={{api_key}}"
