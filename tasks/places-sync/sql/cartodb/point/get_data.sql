c71ff293356111ae5b64e89594c3a35cdd41d994
Jim McAndrew <jim@loc8.us>
2015-09-24 18:08:21 -0400

    Reorder & add updated/created columns

--- tasks/places-sync/sql/cartodb/point/get_data.sql
+++ tasks/places-sync/sql/cartodb/point/get_data.sql
@@ -1,15 +1,17 @@
 -- Get all of the updates
 SELECT
   "nps_cartodb_point_view"."cartodb_id",
-  "nps_cartodb_point_view"."version",
-  "nps_cartodb_point_view"."name",
-  "nps_cartodb_point_view"."places_id",
-  "nps_cartodb_point_view"."superclass",
+  ST_AsGeoJSON(ST_Transform("nps_cartodb_point_view"."the_geom", 4326))::text AS "the_geom"
   "nps_cartodb_point_view"."class",
+  "nps_cartodb_point_view"."name",
+  "nps_cartodb_point_view"."places_created_at",
+  "nps_cartodb_point_view"."places_id",
+  "nps_cartodb_point_view"."places_updated_at",
+  "nps_cartodb_point_view"."superclass",
   "nps_cartodb_point_view"."tags",
   "nps_cartodb_point_view"."type",
   "nps_cartodb_point_view"."unit_code",
-  ST_AsGeoJSON(ST_Transform("nps_cartodb_point_view"."the_geom", 4326))::text AS "the_geom"
+  "nps_cartodb_point_view"."version"
 FROM
   "nps_cartodb_point_view"
 WHERE
