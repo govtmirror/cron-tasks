477b74ad282531411283076c40f9c6bfd039c89c
Jim McAndrew <jim@loc8.us>
2015-10-07 12:33:19 -0600

    Update sync tasks

--- tasks/change-within/index.js
+++ tasks/change-within/index.js
@@ -1,3 +1,26 @@
 var config = require('./src/readConfig')();
 
 console.log(config);
+
+// Read the email addresses file
+var file = require('./emailList');
+var readAddresses = function (addresses) {
+  var addressList = addresses.map(function (address) {
+    var returnValue = {};
+    if (address.email) {
+      returnValue.email = address.email;
+      if (address.parks) {
+        returnValue.parks = address.parks;
+      } else {
+        returnValue.parks = [];
+      }
+      if (address.park) {
+        returnValue.parks.push(address.park);
+      }
+    }
+    return returnValue;
+  });
+  return addressList;
+};
+
+console.log(readAddresses(file));
