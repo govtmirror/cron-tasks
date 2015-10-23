76bb3808a038456b070195432a39e3c97cd5e93a
Jim McAndrew <jim@loc8.us>
2015-10-23 08:02:30 -0600

    Update task list

--- taskList.js
+++ taskList.js
@@ -32,51 +32,13 @@ module.exports = function (params) {
       'path': handlebars('/bin/bash {{tasksDir}}/places-backup/update_geojson.sh')
     }
   }, {
-    'description': 'Compile json files without images for Places Mobile',
-    'comment': 'This runs much quicker than the version with images',
-    'enabled': false,
-    'interval': '0 */25 * * * *', // Every 25 minutes
-    'name': 'Places_Mobile_Without_Images',
-    'task': {
-      'type': 'script',
-      'path': handlebars('/bin/bash {{tasksDir}}/places-mobile/compile_locations.sh 0')
-    }
-  }, {
-    'description': 'Compile json files and images for Places Mobile',
-    'comment': 'This requires going out to mapbox and checking off the the map thumbnails, so it takes longer',
-    'enabled': false,
-    'interval': '0 45 */3 * * *', // Every 3 hours on the 45
-    'name': 'Places_Mobile_Images',
-    'task': {
-      'type': 'script',
-      'path': handlebars('/bin/bash {{tasksDir}}/places-mobile/compile_locations.sh map_thumbnail')
-    }
-  }, {
     'description': 'Syncs the places points, polygons, and lines with CartoDB',
     'enabled': true,
     'interval': '0 * * * * *', // Every minute
     'name': 'CartoDB_Render',
     'task': {
       'type': 'script',
-      'path': handlebars('/usr/bin/node /home/npmap/dev/cron-tasks/tasks/places-sync/index.js')
-    }
-  }, {
-    'description': 'Syncs the places points with Mapbox (nps.places_poi)',
-    'enabled': false,
-    'interval': '0 15 * * * *', // Every 1 hour on the 15
-    'name': 'POI Tile Sync POI',
-    'task': {
-      'type': 'script',
-      'path': '/bin/bash /home/npmap/dev/cron-tasks/tasks/mbtiles-sync/run-mbstudio.sh nps_park_pois'
-    }
-  }, {
-    'description': 'Syncs all of the places data with Mapbox (nps.nps-places-data)',
-    'enabled': false,
-    'interval': '0 45 * * * *', // Every 1 hour on the 45
-    'name': 'POI Tile Sync Data',
-    'task': {
-      'type': 'script',
-      'path': '/bin/bash /home/npmap/dev/cron-tasks/tasks/mbtiles-sync/run-mbstudio.sh nps_places_data'
+      'path': handlebars('/usr/bin/node {{tasksDir}}/places-sync/index.js')
     }
   }];
 };
