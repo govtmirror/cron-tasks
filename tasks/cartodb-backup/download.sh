dff8421b773c1df7d2d4e961f656b08a7dd46e57
Jim McAndrew <jim@loc8.us>
2016-09-26 08:42:21 -0600

    Use script directory input

--- tasks/cartodb-backup/download.sh
+++ tasks/cartodb-backup/download.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # CartoDB Download Tool
-
+scriptDir="/home/npmap/dev/cron-tasks/tasks/cartodb-backup"
 function downloadTables ()
 {
-  source "./settings.sh"
+  source $scriptDir"/settings.sh"
   local destination=$CartoDB_destination"_"$(date +'%Y%m%d')
   local file_type=$CartoDB_file_type
   # local tables=$(cat $CartoDB_table_list_file)
@@ -12,6 +12,7 @@ function downloadTables ()
   local account=""
   local file_name=""
   local table_name=""
+  local CartoDB_api_key_file=$scriptDir$CartoDB_api_key_file
 
 
   function  downloadTable()
@@ -34,9 +35,9 @@ function downloadTables ()
     account=$(echo $line | perl -pe "s/^(.+?),\s{0,}(.+?)$/\1/g")
     table_name=$(echo $line | perl -pe "s/^(.+?),\s{0,}(.+?)$/\2/g")
     api_key=$(grep -r "^$account," "$CartoDB_api_key_file" | perl -pe "s/^(.+?),\s{0,}(.+?)$/\2/g")
-    file_name="$account".$table_name"."$file_type
+    file_name=$account"."$table_name"."$file_type
     downloadTable
-  done < "$CartoDB_table_list_file"
+  done < "$scriptDir""$CartoDB_table_list_file"
   zip -r $destination".zip" $destination && rm -r $destination
   echo "Completed"
 }
