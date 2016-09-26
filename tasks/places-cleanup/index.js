c8e8b376d3b88eab5b89a096062d8deff337b569
Jim McAndrew <jim@loc8.us>
2016-09-26 08:50:08 -0600

    Add places cleanup scripts

---
+++ tasks/places-cleanup/index.js
@@ -0,0 +1,24 @@
+var Databases = require('places-sync-databases');
+var config = require('config');
+var db = new Databases({
+  'type': 'postgresql',
+  'connection': config.connection
+});
+var fs = require('fs');
+var files = {
+  elements: 'fix_missing_elements.sql',
+  versions: 'fix_missing_versions.sql',
+  tags: 'update_tags.sql'
+};
+var promises = [];
+
+for (var file in files) {
+  files[file] = fs.readFileSync('./sql/' + files[file], 'utf8');
+  promises.push(db.query(files[file]));
+}
+
+Promise.all(promises).then(function(resp) {
+  console.log('resp', resp);
+}).catch(function(e) {
+  throw e;
+});
